fe9de2f02c486d8387c718fd5daf15a13860e7c4
William <will.liiam06@gmail.com>
2024-12-04 15:54:24 +0700

    fix: top padding

--- lib/features/homepage/presentation/homepage.dart
+++ lib/features/homepage/presentation/homepage.dart
@@ -8,12 +8,15 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: const [
-            Padding(padding: EdgeInsets.all(16.0)),
-            ProfileListView()
-          ],
+        child: Padding(
+          padding: const EdgeInsets.only(top: 16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: const [
+              Padding(padding: EdgeInsets.all(16.0)),
+              ProfileListView()
+            ],
+          ),
         ),
       ),
     );
