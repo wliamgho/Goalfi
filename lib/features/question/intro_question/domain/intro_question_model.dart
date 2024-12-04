92faa310525ada32129babe8168fbea2b59be1b1
William <will.liiam06@gmail.com>
2024-12-04 22:56:52 +0700

    feat: load intro question to loaded in profile card

--- lib/features/question/intro_question/domain/intro_question_model.dart
+++ lib/features/question/intro_question/domain/intro_question_model.dart
@@ -1,6 +1,32 @@
-class IntroQuestion {
+import 'package:shared_preferences/shared_preferences.dart';
+
+class IntroQuestionData {
   final String name;
   final int age;
 
-  IntroQuestion({required this.name, required this.age});
+  IntroQuestionData({required this.name, required this.age});
+
+  // Convert object to a map
+  Map<String, dynamic> toMap() {
+    return {'name': name, 'age': age};
+  }
+
+  // Save to SharedPreferences
+  Future<void> saveToPrefs() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString('name', name);
+    await prefs.setInt('age', age);
+  }
+
+  // Load from SharedPreferences
+  static Future<IntroQuestionData?> loadFromPrefs() async {
+    final prefs = await SharedPreferences.getInstance();
+    final String? name = prefs.getString('name');
+    final int? age = prefs.getInt('age');
+
+    if (name != null && age != null) {
+      return IntroQuestionData(name: name, age: age);
+    }
+    return null; // Return null if data is incomplete
+  }
 }
